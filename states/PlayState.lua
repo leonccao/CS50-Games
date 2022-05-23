43bb90ad9ba002f88080f8c59472646b3f8a5153
Leon Cao <leonccao@gmail.com>
2022-05-22 17:49:26 -0700

    Add random horizontal gaps between gaps

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -25,6 +25,8 @@ function PlayState:init()
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+    self.randomHorizontalGap(self)
 end
 
 function PlayState:update(dt)
@@ -32,7 +34,7 @@ function PlayState:update(dt)
     self.timer = self.timer + dt
 
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer > self.widthBetweenPipes then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
@@ -45,6 +47,8 @@ function PlayState:update(dt)
 
         -- reset timer
         self.timer = 0
+        
+        self.randomHorizontalGap(self)
     end
 
     -- for every pair of pipes..
@@ -126,4 +130,12 @@ end
 function PlayState:exit()
     -- stop scrolling for the death/score screen
     scrolling = false
+end
+
+--[[
+    Re-random the horizontal gaps between pipes.
+]]
+function PlayState:randomHorizontalGap()
+    local temp = math.min(self.score / 8, 5) + 1
+    self.widthBetweenPipes = 1.5 + 1 / temp + 0.7 * math.random()
 end
