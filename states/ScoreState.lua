84146ae920a792de6fbc79fba88123648a0e68e6
Leon Cao <leonccao@gmail.com>
2022-05-22 21:33:06 -0700

    Add medals for different score levels

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -10,6 +10,10 @@
 
 ScoreState = Class{__includes = BaseState}
 
+local goldMedalImage = love.graphics.newImage('resources/images/gold_medal.png')
+local silverMedalImage = love.graphics.newImage('resources/images/silver_medal.png')
+local bronzeMedalImage = love.graphics.newImage('resources/images/bronze_medal.png')
+
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
@@ -27,11 +31,28 @@ end
 
 function ScoreState:render()
     -- simply render the score to the middle of the screen
+    local resultText = 'Oof! You Lost!'
+    local medalImage = nil
+    if self.score >= 20 then
+        resultText = 'Perfect!'
+        medalImage = goldMedalImage
+    elseif self.score >= 10 then
+        resultText = 'Well Done!' 
+        medalImage = silverMedalImage
+    elseif self.score >= 5 then
+        resultText = 'Good Job!'
+        medalImage = bronzeMedalImage
+    end
+
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf(resultText, 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+    if medalImage ~= nil then
+        love.graphics.draw(medalImage, VIRTUAL_WIDTH / 2 - 32, 120)
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 190, VIRTUAL_WIDTH, 'center')
 end
